7ead63e27c7bfe1d4a91d40452a9660eba6f370a
Gennadiy Gonchar <gennagon@ukr.net>
2016-05-06 12:51:42 +0300

    первый shop на MVC

---
+++ models/ProductsModel.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Модель для таблицы продукции (products)
+ * 
+ */
+
+/**
+ * Получаем последние добавленные товары
+ * 
+ * @param integer $limit Лимит товаров
+ * @return array Массив товаров 
+ */
+function getLastProducts($limit = null)
+{
+    $sql = "SELECT *
+            FROM `products` 
+            ORDER BY id DESC";
+    if($limit){
+        $sql .= " LIMIT {$limit}";
+    }
+   
+    $rs = mysql_query($sql); 
+   
+   return createSmartyRsArray($rs); 
+}
+
+/**
+ * Получить продукты для категории $itemId
+ * 
+ * @param integer $itemId ID категории
+ * @return array массив продуктов 
+ */
+function getProductsByCat($itemId)
+{
+   $itemId = intval($itemId);
+   $sql = "SELECT * 
+            FROM products
+            WHERE category_id = '{$itemId}'";
+   
+   $rs = mysql_query($sql); 
+   
+   return createSmartyRsArray($rs);   
+}
+
+
+/**
+ * Получить данные продукта по ID 
+ * 
+ * @param integer $itemId ID продукта
+ * @return array массив данных продукта 
+ */
+function getProductById($itemId)
+{
+   $itemId = intval($itemId);
+   $sql = "SELECT * 
+            FROM products
+            WHERE id = '{$itemId}'";
+   
+   $rs = mysql_query($sql); 
+   return mysql_fetch_assoc($rs);   
+}
+
+/**
+ * Получить список продуктов из массива идентификаторов (ID`s)
+ * 
+ * @param array $itemsIds массив идентификаторов продуктов
+ * @return array массив данных продуктов 
+ */
+function getProductsFromArray($itemsIds)
+{
+    $strIds = implode($itemsIds, ', ');
+    $sql = "SELECT * 
+            FROM products
+            WHERE id in ({$strIds})";		
+    $rs = mysql_query($sql); 
+   
+   return createSmartyRsArray($rs); 
+}
